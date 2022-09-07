612c9e89cd4645f4c001fd6f9391b4c41033c04a
Adrian Dapprich <adrian@dappri.ch>
2022-09-08 00:35:46 +0200

    add db template

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,14 @@ if [ -f "stechuhr.sqlite3" ]; then
     rm stechuhr.sqlite3
 fi
 
-cat migrations/**/up.sql | sqlite3 stechuhr.sqlite3
-cargo run --bin add_6am_events
+if [ -f "stechuhr.sqlite3.template" ]; then
+    cp stechuhr.sqlite3.template stechuhr.sqlite3
+else
+    cat migrations/**/up.sql | sqlite3 stechuhr.sqlite3
+    cargo run --bin add_6am_events
+
+    cp stechuhr.sqlite3 stechuhr.sqlite3.template
+fi
 
 mkdir -p auswertung
 
